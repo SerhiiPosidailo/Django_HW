68974081ee1f2fd75d1d8bab1f2dd2986a6735e4
Notebook <zekserj@ukr.net>
2024-01-23 17:24:32 +0200

    hw_2_FIX_order_by

--- apps/cars/filters.py
+++ apps/cars/filters.py
@@ -4,12 +4,12 @@ from django.http import QueryDict
 from .models import CarModel
 
 
-def cars_filter(query:QueryDict) -> QuerySet:
+def cars_filter(query: QueryDict) -> QuerySet:
     qs = CarModel.objects.all()
     for k, v in query.items():
         match k:
             case 'year_lt':
-                qs = qs.filter(year__lt=v).order_by('year')
+                qs = qs.filter(year__lt=v)
             case 'year__gt':
                 qs = qs.filter(year__gt=v)
             case 'year_gte':
@@ -17,10 +17,14 @@ def cars_filter(query:QueryDict) -> QuerySet:
             case 'year__lte':
                 qs = qs.filter(year__lte=v)
             case 'model__iendswith':
-                qs = qs.filter(model__iendswith=v).order_by('-model')
+                qs = qs.filter(model__iendswith=v)
             case 'model__icontains':
                 qs = qs.filter(model__icontains =v)
             case 'model__istartswith':
                 qs = qs.filter(model__istartswith=v)
+            case 'sort':
+                qs = qs.order_by(v)
+            case 'sort_max':
+                qs = qs.order_by(f'-{v}')
 
     return qs
