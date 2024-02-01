77f29eee6a9f212f121cd2a53a9440ccd3640320
Notebook <zekserj@ukr.net>
2024-02-01 13:47:06 +0200

    hw_4

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,37 +1,13 @@
-from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
 
-from apps.users.models import ProfileModel
-
-UserModel = get_user_model()
-
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProfileModel
-        fields = ('id', 'name', 'surname', 'age', 'updated_at', 'created_at')
-        read_only_fields = ('id', 'created_at', 'updated_at')
+from apps.auto_parks.serializers import AutoParkWithOutCarsSerializer
+from apps.users.models import UserModel
 
 
 class UserSerializer(serializers.ModelSerializer):
+    auto_parks = AutoParkWithOutCarsSerializer(many=True, read_only=True)
+
     class Meta:
         model = UserModel
-        fields = (
-            'id', 'email', 'password', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'created_at',
-            'updated_at', 'profile'
-        )
-        read_only_fields = ('id', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'created_at', 'updated_at')
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
-        }
-
-    def create(self, validated_data):
-        profile = validated_data.pop('profile')
-        profile = ProfileModel.objects.create(**profile)
-        user = UserModel.objects.create_user(profile=profile, **validated_data)
-        return user
-
+        fields = ('id', 'name', 'age', 'updated_at', 'created_at', 'auto_parks')
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,25 +1,31 @@
-from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 
-from rest_framework.generics import CreateAPIView, ListCreateAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from apps.auto_parks.serializers import AutoParkSerializer
+from apps.users.models import UserModel
 from apps.users.serializers import UserSerializer
 
-UserModel = get_user_model()
 
-
-class UserCreateView(CreateAPIView):
+class UsersListCreateView(ListCreateAPIView):
+    queryset = UserModel.objects.all()
     serializer_class = UserSerializer
 
 
-class UserCreateView(ListCreateAPIView):
+class UsersRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = UserModel.objects.all()
     serializer_class = UserSerializer
 
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return (IsAuthenticated(),)
-        return (AllowAny(),)
 
-    def get_queryset(self):
-        return UserModel.objects.exclude(pk=self.request.user.pk)
+class UsersAddAutoParkView(GenericAPIView):
+    queryset = UserModel.objects.all()
+
+    def post(self, *args, **kwargs):
+        user = self.get_object()
+        data = self.request.data
+        serializer = AutoParkSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user)
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data, status.HTTP_201_CREATED)
 
