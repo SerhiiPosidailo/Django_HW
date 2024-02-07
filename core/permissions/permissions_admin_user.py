d3bf4283170817525088e70d2b4c3e9bb64986a7
Notebook <zekserj@ukr.net>
2024-02-07 09:49:17 +0200

    hw_6

--- core/permissions/permissions_admin_user.py
+++ core/permissions/permissions_admin_user.py
@@ -3,5 +3,5 @@ from rest_framework.permissions import BasePermission
 
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_staff)
+        return bool(request.user and request.user.is_staff and request.user.is_superuser)
 
