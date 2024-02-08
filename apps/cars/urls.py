0feed1c64df6da24771bbab71914a6c964d2f28c
Notebook <zekserj@ukr.net>
2024-02-08 08:51:08 +0200

    hw_6

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -5,5 +5,5 @@ from .views import CarAddAvatarView, CarListCreateView, CarsRetriveUpdateDestroy
 urlpatterns = [
     path('', CarListCreateView.as_view(), name='car_list_create'),
     path('/<int:pk>', CarsRetriveUpdateDestroyView.as_view(), name='cars_retrieve_update_destroy'),
-path('/<int:pk>/avatar', CarAddAvatarView.as_view(), name='cars_add_photo')
+    path('/<int:pk>/avatar', CarAddAvatarView.as_view(), name='cars_add_photo')
 ]
